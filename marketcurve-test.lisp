d541d89128e5fb937b9b081ae04b8769b95e147c
Wilson Tam <wilson@Wilsons-Mac-mini-2.local>
2015-05-02 16:32:38 +0800

    first commit

---
+++ marketcurve-test.lisp
@@ -0,0 +1,63 @@
+(declaim (optimize (speed 0) (safety 3) (debug 3)))
+
+;;;;
+;;;; Tests for Marketbus
+;;;;
+
+(defpackage :marketbus-test
+  (:use :common-lisp
+	:marketbus
+	:lisp-unit))
+
+(in-package :marketbus-test)
+
+
+;;; Global variables
+
+;;;
+;;; Tests
+;;;
+
+(define-test test-integer-scale
+  (let ((scale (new-scale "~a" '(1 2 3 4 5))))
+    (assert-equal t   (valid-scale-point scale 1))
+    (assert-equal nil (valid-scale-point scale 9))
+    (assert-equal nil (valid-scale-point scale "a"))
+    (assert-equal 3   (prev-scale-point scale 4))
+    (assert-equal 2   (next-scale-point scale 1))
+    (assert-equal 1   (first-scale-point scale))
+    (assert-equal 5   (last-scale-point scale))
+    (assert-equal nil (prev-scale-point scale 1))
+    (assert-equal nil (next-scale-point scale 5))))
+
+(define-test test-symbol-scale
+  (let ((scale (new-scale "~a" '(F G H J K M N Q U V X Z))))
+    (assert-equal t   (valid-scale-point scale 'F))
+    (assert-equal nil (valid-scale-point scale 9))
+    (assert-equal nil (valid-scale-point scale "a"))
+    (assert-equal 'F  (prev-scale-point scale 'G))
+    (assert-equal 'X  (next-scale-point scale 'V))
+    (assert-equal 'F  (first-scale-point scale))
+    (assert-equal 'Z  (last-scale-point scale))
+    (assert-equal nil (prev-scale-point scale 'F))
+    (assert-equal nil (next-scale-point scale 'Z))))
+
+(define-test test-2-level-symbol-scale)
+(let ((scale (new-scale "??"
+			'(F G H J K M N Q U V X Z)
+			'(10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25)))
+    (assert-equal t   (valid-scale-point scale 'F))
+    (assert-equal nil (valid-scale-point scale 9))
+    (assert-equal nil (valid-scale-point scale "a"))
+    (assert-equal 'F  (prev-scale-point scale 'G))
+    (assert-equal 'X  (next-scale-point scale 'V))
+    (assert-equal 'F  (first-scale-point scale))
+    (assert-equal 'Z  (last-scale-point scale))
+    (assert-equal nil (prev-scale-point scale 'F))
+    (assert-equal nil (next-scale-point scale 'Z))))
+  
+;;;
+;;; Run all test
+;;;
+(run-tests :all)
+
---
+++ marketcurve.lisp
@@ -0,0 +1,73 @@
+(declaim (optimize (speed 0) (safety 3) (debug 3)))
+
+;;;;
+;;;; Create a Mark Curve store and retieveing system, with formula feature
+;;;;
+
+(defpackage :marketbus
+  (:use :common-lisp)
+  (:export :new-scale
+	   :add-scale
+	   :load-curve
+	   :new-manual-curve
+	   :first-scale-point
+	   :last-scale-point
+	   :valid-scale-point
+	   :prev-scale-point
+	   :next-scale-point))
+
+(in-package :marketbus)
+
+;;;
+;;; Global variables
+;;;
+(defvar *marketbus* nil)
+(defvar *curve-scales* nil)
+
+
+;;;
+;;; Scales
+;;;
+(defun new-scale (name sequence)
+  (list :name name :seq sequence))
+  
+(defun add-scale (scale)
+  (push scale *curve-scales*))
+
+(defun first-scale-point (scale)
+  (first (getf scale :seq)))
+
+(defun last-scale-point (scale)
+  (first (last (getf scale :seq))))
+
+(defun valid-scale-point (scale point)
+  (numberp (position point (getf scale :seq))))
+
+(defun prev-scale-point (scale point)
+  (let* ((scale-seq (getf scale :seq))
+	(pos (position point scale-seq)))
+    (and pos
+	 (> pos 0)
+	 (nth (- pos 1) scale-seq))))
+
+(defun next-scale-point (scale point)
+  (let* ((scale-seq (getf scale :seq))
+	(pos (position point scale-seq)))
+    (and pos
+	 (nth (+ pos 1) scale-seq))))
+
+
+;;;
+;;; Curves Operation
+;;;
+(defun load-curve (name)
+  "Load indiviual curve by name"
+  (remove-if-not
+   #'(lambda (curve) (equal (getf curve :name) name))
+   *marketbus*))
+
+
+(defun new-manual-curve (name scale start-point end-point)
+  (list :name name :scale scale :start-point start-point :end-point end-point))
+
+
